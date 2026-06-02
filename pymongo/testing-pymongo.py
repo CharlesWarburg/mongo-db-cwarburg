717fa091e2ccef6c8a5e8c6001aee46b7129d001
Charlie <251178885+CharlesWarburg@users.noreply.github.com>
2026-06-02 15:11:23 +0100

    Delete pymongo directory

--- pymongo/testing-pymongo.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["starwars"]
-people = db["people"]
-result = people.find_one({"name": "Luke Skywalker"})
-print(result)
